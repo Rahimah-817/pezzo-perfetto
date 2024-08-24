b6367fcbf07a82db10ce6d531085efd23732237a
Rahimah Ansari <rahimeansari7@gmail.com>
2024-08-24 09:46:49 +0430

    connected the mongodb db with our app

---
+++ src/app/api/register/route.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+import { User } from '@/models/User';
+
+export async function POST(req) {
+  mongoose.connect(process.env.MONGO_URL);
+
+  const body = await req.json();
+
+  const createUser = await User.create(body);
+
+  return Response.json(createUser);
+}
